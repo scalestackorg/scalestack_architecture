ebb2983ca262fc507e12d29df4ff7aa0a5ddb74a
Carlos Albornoz <calbornoz@scalestack.ai>
2024-05-11 06:00:09 -0300

    fix: remove partial import

--- scalestack_architecture/__init__.py
+++ scalestack_architecture/__init__.py
@@ -1,3 +1,7 @@
-from factories.api_gateway import ApiGatewayFactory as ApiGatewayFactory
-from factories.datadog import DatadogFactory as DatadogFactory
-from factories.functions import PythonLambdaFactory as PythonLambdaFactory
+from scalestack_architecture.factories.api_gateway import (
+    ApiGatewayFactory as ApiGatewayFactory,
+)
+from scalestack_architecture.factories.datadog import DatadogFactory as DatadogFactory
+from scalestack_architecture.factories.functions import (
+    PythonLambdaFactory as PythonLambdaFactory,
+)
