5c9a23e00f8faffa933093f60734d9acdb7b4380
Carlos Albornoz <calbornoz@scalestack.ai>
2024-05-21 14:26:59 -0300

    fix: added optional scope

--- scalestack_architecture/factories/base.py
+++ scalestack_architecture/factories/base.py
@@ -3,11 +3,13 @@ from constructs import Construct
 
 
 class BaseFactory:
-    def __init__(self, stack: Stack, stage: str, prefix: str, scope: Construct):
+    def __init__(self, stack: Stack, stage: str, prefix: str, scope: Construct = None):
         self.stack = stack
         self.stage = stage
         self.prefix = prefix
-        self.scope = scope
+        self.scope = (
+            scope or stack
+        )  # if the scope (parent stack) is not provided, use the stack itself
 
     def name(self, name: str):
         if self.prefix:
