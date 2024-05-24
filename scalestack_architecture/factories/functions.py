6ee0b76f5d9590c0363ef08a90d1633c55d71eec
Carlos Albornoz <calbornoz@scalestack.ai>
2024-05-23 23:04:44 -0300

    fix: changed logging to json

--- scalestack_architecture/factories/functions.py
+++ scalestack_architecture/factories/functions.py
@@ -149,6 +149,7 @@ class PythonLambdaFactory(BaseFactory):
             memory_size=memory_size,
             layers=layers,
             initial_policy=policies,
+            logging_format=lambda_.LogFormat.JSON,
         )
         self.created_functions[name] = func
         CfnOutput(
